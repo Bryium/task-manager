d4f16a43459077f723b66ed5b3e311c8f457f479
Bryium <bryiumonyancha@gmail.com>
2025-07-14 18:19:32 +0300

    defined task models

---
+++ backend/app/models/task.py
@@ -0,0 +1,11 @@
+from .. import db
+from datetime import datetime
+
+class Task(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(150), nullable=False)
+    description = db.Column(db.Text)
+    status = db.Column(db.String(20), default='Pending')  # Pending, In Progress, Completed
+    deadline = db.Column(db.DateTime, nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
