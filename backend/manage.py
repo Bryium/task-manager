ff3762e867aaaf38ecc1acb0aeba5b9a0d510ae1
Bryium <bryiumonyancha@gmail.com>
2025-07-15 03:05:11 +0300

    update the listening ports

--- backend/manage.py
+++ backend/manage.py
@@ -1,9 +1,12 @@
 # manage.py
 from flask.cli import FlaskGroup
 from app import create_app
+import os
 from app.extensions import db, migrate
 
 app = create_app()
 
 if __name__ == "__main__":
-    app.run()
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
+    
